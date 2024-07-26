056e0798cad609b2a10c99d222531e44c509f966
Bankole Ojo <ojobankole@gmail.com>
2024-07-26 03:00:05 +0100

    refactoring

--- audio.go
+++ audio.go
@@ -11,35 +11,6 @@ import (
 	"github.com/saenuma/lyrics818/l8f"
 )
 
-// func GetMPCommand() string {
-// 	var cmdPath string
-// 	begin := os.Getenv("SNAP")
-// 	cmdPath = "madplay"
-// 	if begin != "" && !strings.HasPrefix(begin, "/snap/go/") {
-// 		cmdPath = filepath.Join(begin, "usr", "bin", "madplay")
-// 	}
-
-// 	return cmdPath
-// }
-
-// func playAudio(l8fPath, seek string) {
-// 	rootPath, _ := GetRootPath()
-// 	mplayCmd := GetMPCommand()
-// 	ctx, cancel := context.WithCancel(context.Background())
-// 	defer cancel() // cancel when we are finished consuming integers
-
-// 	audioBytes, err := l8f.ReadAudio(l8fPath)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	tmpAudioPath := filepath.Join(rootPath, ".tmp_audio.mp3")
-// 	os.WriteFile(tmpAudioPath, audioBytes, 0777)
-
-// 	playerCancelFn = cancel
-// 	exec.CommandContext(ctx, mplayCmd, "-s", seek, tmpAudioPath).Run()
-// }
-
 var GlobalOtoCtx *oto.Context
 var currentPlayer *oto.Player
 
